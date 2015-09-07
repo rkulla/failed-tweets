5ae158569e00d2d8b8354d0e2fac74899c33abba
Ryan Kulla <rkulla@gmail.com>
2015-09-07 13:31:51 -0700

    Fix indentation

--- index.js
+++ index.js
@@ -6,25 +6,26 @@ require('dotenv').load();
 var Twitter = require('twitter');
 
 var client = new Twitter({
-  consumer_key: process.env.CONSUMER_KEY,
-  consumer_secret: process.env.CONSUMER_SECRET,
-  access_token_key: process.env.ACCESS_TOKEN_KEY,
-  access_token_secret: process.env.ACCESS_TOKEN_SECRET
+    consumer_key: process.env.CONSUMER_KEY,
+    consumer_secret: process.env.CONSUMER_SECRET,
+    access_token_key: process.env.ACCESS_TOKEN_KEY,
+    access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
 // View the most recent failed tweets
 var params = {
-  screen_name: process.env.USER_NAME,
-  count: 100 // max
+    screen_name: process.env.USER_NAME,
+    count: 100 // max
 },
 tweet;
-client.get('statuses/user_timeline', params, function(error, tweets, response){
-  if (!error) {
-    for (var i = 0, n = tweets.length; i < n; i++) {
-      tweet = tweets[i];
-      if (tweet.favorite_count < 1) {
-         console.log(tweet.text);
-      }
+
+client.get('statuses/user_timeline', params, function(error, tweets) {
+    if (!error) {
+        for (var i = 0, n = tweets.length; i < n; i++) {
+            tweet = tweets[i];
+            if (tweet.favorite_count < 1) {
+                console.log(tweet.text);
+            }
+        }
     }
-  }
 });
