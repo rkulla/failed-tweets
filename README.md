bfd3df0e762665fd1b53a9b1a2fb5458606325fc
Ryan Kulla <rkulla@gmail.com>
2015-09-07 21:33:49 -0700

    Only show tweets that weren't replied to either

--- README.md
+++ README.md
@@ -1,3 +1,17 @@
+Failed-tweets is a simple command line app that shows you any tweets that you,
+or your organization, didn't get any favorites, retweets, or replies to.
+
+Of course, these aren't really "failed" tweets, but if might be if you were
+aiming for a response and didn't get one.
+
+This can be useful for helping you decide to try tweeting those tweets again
+or just deleting them.
+
+Note that it doesn't take into account if there was a lot of activity on a
+tweet, which might still mean people read it or clicked it. So take this
+with a grain of salt.
+
+### Usage
 Create a file called .env in the root of this project directory and add:
 
     # Twitter OAuth credentials
@@ -13,3 +27,7 @@ Fill in the values with your twitter credentials by going to
 https://apps.twitter.com/app/new and creating a `failed-tweets` 
 app with read/write perms.
 
+
+Run the program with:
+
+    $ node index.js
--- index.js
+++ index.js
@@ -12,20 +12,87 @@ var client = new Twitter({
     access_token_secret: process.env.ACCESS_TOKEN_SECRET
 });
 
-// View the most recent failed tweets
+var userPath = '/statuses/user_timeline',
+    mentionsPath = '/statuses/mentions_timeline';
+
 var params = {
     screen_name: process.env.USER_NAME,
-    count: 100 // max
-},
-tweet;
+    count: 100
+};
 
-client.get('statuses/user_timeline', params, function(error, tweets) {
-    if (!error) {
-        for (var i = 0, n = tweets.length; i < n; i++) {
-            tweet = tweets[i];
-            if (tweet.favorite_count < 1) {
-                console.log(tweet.text);
-            }
+// Check our rate limit requests remaining first
+client.get('application/rate_limit_status',
+{ resources: 'statuses' },
+function(err, data) {
+
+    if (!err) {
+        var stats = data.resources.statuses,
+            userLeft = stats[userPath].remaining,
+            mentionsLeft = stats[mentionsPath].remaining;
+
+        console.log('User Requests Left =', userLeft);
+        console.log('Mentions Requests Left =', mentionsLeft);
+
+        if (userLeft === 0 || mentionsLeft === 0) {
+            console.log('Rate limit exceeded. Try again in 15 minutes.');
+            process.exit(0);
         }
+    }
+
+});
+
+// A flag for if every test for failure happens
+var failed = false;
+
+// Get all the mentions up front
+client.get(mentionsPath, params, function(err, mentions) {
+    if (!err) {
+        console.log('Nobody favorited/retweeted/replied to these tweets:');
+        console.log();
+
+        // Get the most recent tweets
+        client.get(userPath, params, function(err2, tweets) {
+            if (!err2) {
+
+                // Loop through the tweets
+                for (var i = 0, n = tweets.length; i < n; i++) {
+
+                    // Check if tweet wasn't favorited or retweeted
+                    // and wasn't just a reply to someone
+                    if (tweets[i].favorite_count === 0 &&
+                        tweets[i].retweet_count === 0 &&
+                        tweets[i].in_reply_to_status_id_str === null) {
+
+                        // Loop through the mentions
+                        for (var i2 = 0, n2 = mentions.length; i2 < n2; i2++) {
+
+                            // Check if the tweet was in any mentions
+                            if (tweets[i].id_str ===
+                                mentions[i2].in_reply_to_status_id_str) {
+
+                                // Don't count replying to yourself
+                                if (mentions[i2].in_reply_to_user_id_str ===
+                                    mentions[i2].user.id_str) {
+
+                                    failed = true;
+                                    break;
+                                }
+                            }
+
+                        }
+
+                        if (failed) {
+                            console.log('https://twitter.com/' +
+                                process.env.USER_NAME + '/status/' +
+                                tweets[i].id_str);
+
+                            console.log(tweets[i].text);
+                            console.log();
+                        }
+                    }
+                }
+            }
+        });
+
     }
 });
